bd5dcd00c51c841bcdbd33f3071b76ffc63d61d8
Ayomide Bakare <bakkareayomideo@gmail.com>
2020-07-20 13:56:45 +0100

    remove typings | add missing functions

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,18 @@
-const keepDefinedValues = (data: { [key: string]: any } | any) => {
+const isEmpty = (obj) => {
+  return (
+    obj === undefined ||
+    obj === null ||
+    (typeof obj === 'object' && Object.keys(obj).length === 0) ||
+    (Array.isArray(obj) && obj.length === 0) ||
+    (typeof obj === 'string' && obj.trim().length === 0)
+  );
+};
+
+const isEmptyObject = (obj) => {
+  return !(typeof obj === 'object' && Object.keys(obj).length > 0);
+};
+
+const keepDefinedValues = (data) => {
   if (!isEmpty(data)) {
     return Object.entries(data).reduce((acc, [key, value]) => {
       if (
@@ -17,5 +31,7 @@ const keepDefinedValues = (data: { [key: string]: any } | any) => {
 };
 
 module.exports = {
+  isEmpty,
+  isEmptyObject,
   keepDefinedValues,
 };
